3b0e4ae3b4a34eaa98bd4a0a2f8197b2647541c7
arif-tegar <arif_karim_33rpl@student.smktelkom-mlg.sch.id>
2025-10-15 13:45:13 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,12 @@
+str_numbers = "456"
+str_numbers_to_int = int(str_numbers)
+print("Before casting :", str_numbers, ", the data type is",
+type(str_numbers))
+print("After casting :", str_numbers_to_int, ", the data type is",
+type(str_numbers_to_int))
+
+integer = 12345
+integer_to_str = str(integer)
+print("Before casting :", integer, ", the data type is", type(integer))
+print("After casting :", integer_to_str, ", the data type is",
+type(integer_to_str))
